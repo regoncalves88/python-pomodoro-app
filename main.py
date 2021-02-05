44f48f99cf4412d8986f1b5d6771c6028ac15ba0
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-05 19:58:43 -0300

    Create the window ,add background and write the timer in the screen.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""---Imports---"""
+from tkinter import *
+
 """---Constants---"""
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,3 +18,14 @@ LONG_BREAK_MIN = 20
 """---Countdown Mechanism---"""
 
 """---UI Setup---"""
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
