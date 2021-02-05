10ceffcc62813a1f5e5636c44732034335eb1a98
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-05 20:54:41 -0300

    Create the timer, start it by the Start Button,  and print in the screen.

--- main.py
+++ main.py
@@ -11,11 +11,31 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 """---Timer Reset---"""
 
 """---Timer Mechanism---"""
+def start_timer():
+    countdown(1 * 60)
+
 
 """---Countdown Mechanism---"""
+def time_format(time):
+    if 0 <= time <= 9:
+        return f"0{time}"
+    return time
+
+
+def countdown(count):
+    minutes = int(count / 60)
+    minutes = time_format(minutes)
+    seconds = count % 60
+    seconds = time_format(seconds)
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 """---UI Setup---"""
 window = Tk()
@@ -25,13 +45,13 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
